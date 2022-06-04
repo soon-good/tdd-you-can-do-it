dc2258ca7ba3ab860bc4bdfe68f3419490876744
soon.good <playmymind@daum.net>
2022-06-04 21:05:19 +0900

    웹플럭스 Web계층 테스트코드 프로젝트
    정리하고 있는 문서에 예제를 추가하기 위해 추가...

---
+++ example_codes/webflux_study/src/main/java/io/study/tdd/webflux_study/WebfluxStudyApplication.java
@@ -0,0 +1,13 @@
+package io.study.tdd.webflux_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxStudyApplication.class, args);
+	}
+
+}
---
+++ example_codes/webflux_study/src/test/java/io/study/tdd/webflux_study/WebfluxStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.study.tdd.webflux_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
